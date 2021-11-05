f7cb7771786b583e83d2ec6c050a036f6ad9840b
Guillaume Huard Hughes <guillaume.huard.hughes@radio-canada.ca>
2021-11-05 03:18:46 -0400

    upgrade android packages & made it work with the firebase test

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/guillaumehuard/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/guillaumehuard/Projets/Personnelle/flutter_starter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=2.0.0"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
